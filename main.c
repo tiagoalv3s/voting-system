fcfe69bc3f2eab931933f9eda7e568b30f6b82f8
tiagoalv3s <tiagoalv3s@hotmail.com>
2022-12-09 14:15:40 -0300

    Add opening function

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+void opening(){
+    printf("\n|***************************************|\n");
+    printf("|__________ VOTING SYSTEM 1.0 __________|\n");
+    printf("|_______________________________________|\n\n");
+}
+
 int main(){
     int presCandidate1, presCandidate2, presCandidate3, govCandidate1, govCandidate2, userVote;
     
+    opening();
+
     return 0;
 }
