76bc62095020ca7196bd4f70d71214dfe19fc7be
tiagoalv3s <tiagoalv3s@hotmail.com>
2022-12-10 13:33:55 -0300

    Fix messages in presNumbers/govNumbers functions and delete unnecessary ones in main function

--- main.c
+++ main.c
@@ -9,45 +9,67 @@ void opening(){
 }
 
 void presNumbers(){
-    printf("|----------------------|\n");
-    printf("| CANDIDATE\tNUMBER |\n");
-    printf("|----------------------|\n");
-    printf("| Candidate 1\t%6d |\n", 11);
-    printf("| Candidate 2\t%6d |\n", 22);
-    printf("| Candidate 3\t%6d |\n", 33);
-    printf("|----------------------|\n");
+    printf("\t|-----------------------|\n");
+    printf("\t| CANDIDATE\t NUMBER |\n");
+    printf("\t|-----------------------|\n");
+    printf("\t| Candidate 1\t %6d |\n", 11);
+    printf("\t| Candidate 2\t %6d |\n", 22);
+    printf("\t| Candidate 3\t %6d |\n", 33);
+    printf("\t|-----------------------|\n");
 }
 
 void govNumbers(){
-    printf("|----------------------|\n");
-    printf("| CANDIDATE\tNUMBER |\n");
-    printf("|----------------------|\n");
-    printf("| Candidate 1\t%6d |\n", 10);
-    printf("| Candidate 2\t%6d |\n", 20);
-    printf("|----------------------|\n");
+    printf("\n\t|-----------------------|\n");
+    printf("\t| CANDIDATE\t NUMBER |\n");
+    printf("\t|-----------------------|\n");
+    printf("\t| Candidate 1\t %6d |\n", 10);
+    printf("\t| Candidate 2\t %6d |\n", 20);
+    printf("\t|-----------------------|\n");
 }
 
 int votePres(){
     char confirmation;
     do{
-        printf("\nType in the number of your president candidate:\n\n");
+        printf("\nType in the number of your PRESIDENT candidate: ");
         scanf("%d", &userVote);
         switch (userVote)
         {
         case 11:
-            printf("You voted for Candidate 1\n\n");
+            printf("You voted for Candidate 1\n");
             break;
         case 22:
-            printf("You voted for Candidate 2\n\n");
+            printf("You voted for Candidate 2\n");
             break;
         case 33:
-            printf("You voted for Candidate 3\n\n");
+            printf("You voted for Candidate 3\n");
             break;    
         default:
-            printf("You voted NULL\n\n");
+            printf("You voted NULL\n");
             break;
         }
-        printf("Do you confirm that?(Y/N)");
+        printf("Do you confirm that?(Y/N): ");
+        scanf(" %c", &confirmation);
+    }while(confirmation == 78); // Confirmation using ANSI code of N.
+}
+
+int voteGov(){
+    char confirmation;
+    do{
+        printf("\nType in the number of your GOVERNOR candidate: ");
+        scanf("%d", &userVote);
+        switch (userVote)
+        {
+        case 10:
+            printf("You voted for Candidate 1\n");
+            break;
+        case 20:
+            printf("You voted for Candidate 2\n");
+            break;  
+        default:
+            printf("You voted NULL\n");
+            break;
+        }
+        printf("Do you confirm that?(Y/N): ");
         scanf(" %c", &confirmation);
     }while(confirmation == 78); // Confirmation using ANSI code of N.
 }
@@ -58,11 +80,10 @@ int main(){
     opening();
     
     presNumbers();    
-    
     votePres();
-
+    
+    voteGov();
     govNumbers();
-    printf("\nType in the number of your governor candidate:\n\n");
-    scanf("%d\n", &userVote);
+
     return 0;
 }
