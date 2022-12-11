c136b98314cdc19f3381cb4cd219e028fd7065c1
tiagoalv3s <tiagoalv3s@hotmail.com>
2022-12-11 16:11:42 -0300

    Delete unused variables

--- main.c
+++ main.c
@@ -89,9 +89,7 @@ int storeVote(int position){
     fclose(flpt);
 }
 
-int main(){
-    int presCandidate1, presCandidate2, presCandidate3, govCandidate1, govCandidate2;
-    
+int main(){    
     opening();
     
     numbers(0);    
